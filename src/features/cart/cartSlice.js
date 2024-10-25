17d30674e815ae216daca20f46edd9c8962d84d7
Hoang Bui Duc <hoangakiy@gmail.com>
2024-10-25 15:07:38 +0700

    feat: total quantity and price in cart

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -3,27 +3,13 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   // cart: [],
   cart: [
-    {
-      pizzaId: 12,
-      name: 'Mediterranean',
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-    {
-      pizzaId: 6,
-      name: 'Vegetale',
-      quantity: 1,
-      unitPrice: 13,
-      totalPrice: 13,
-    },
-    {
-      pizzaId: 11,
-      name: 'Spinach and Mushroom',
-      quantity: 1,
-      unitPrice: 15,
-      totalPrice: 15,
-    },
+    // {
+    //   pizzaId: 12,
+    //   name: 'Mediterranean',
+    //   quantity: 2,
+    //   unitPrice: 16,
+    //   totalPrice: 32,
+    // },
   ],
 };
 
@@ -68,3 +54,9 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
